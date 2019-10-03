93fe3c4b06a95e98aeb0b3c16fecdbef802b9579
anisMelliti <ansmeliti@gmail.com>
2019-10-03 22:15:31 +0100

    implement the profile front end

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Index from './views/Index';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import RegisterPage from './views/RegisterPage';
+import ProfilePage from './views/ProfilePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' render={() => <Index />} />
-        <Route path='/register' render={() => <RegisterPage />} />
+        <Route exact path='/profile' render={() => <ProfilePage />} />
+        <Route exact path='/register' render={() => <RegisterPage />} />
       </Switch>
     </BrowserRouter>
   );
