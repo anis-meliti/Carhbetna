147e98c2fe66e1d45b9bb960325a5176316d4429
anisMelliti <ansmeliti@gmail.com>
2019-10-03 21:03:28 +0100

    set up the header

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// reactstrap components
+import { Container } from 'reactstrap';
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className='page-header section-dark'
+        style={{
+          backgroundImage:
+            'url(' + require('../../assets/img/antoine-barres.jpg') + ')'
+        }}
+      >
+        <div className='filter' />
+        <div className='content-center'>
+          <Container>
+            <div className='title-brand'>
+              <h1 className='presentation-title'>Carhbetna</h1>
+              <div className='fog-low'>
+                <img alt='...' src={require('../../assets/img/fog-low.png')} />
+              </div>
+              <div className='fog-low right'>
+                <img alt='...' src={require('../../assets/img/fog-low.png')} />
+              </div>
+            </div>
+            <h2 className='presentation-subtitle text-center'>
+              Trouvez toujours ceux qui partagent avec vous vos trajet
+            </h2>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
---
+++ src/views/Index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+// reactstrap components
+
+// core components
+import IndexNavbar from '../components/Headers/Navbars/IndexNavbar';
+import IndexHeader from '../components/Headers/IndexHeader';
+
+// index sections
+// import SectionButtons from "views/index-sections/SectionButtons.js";
+// import SectionNavbars from "views/index-sections/SectionNavbars.js";
+// import SectionNavigation from "views/index-sections/SectionNavigation.js";
+// import SectionProgress from "views/index-sections/SectionProgress.js";
+// import SectionNotifications from "views/index-sections/SectionNotifications.js";
+// import SectionTypography from "views/index-sections/SectionTypography.js";
+// import SectionJavaScript from "views/index-sections/SectionJavaScript.js";
+// import SectionCarousel from "views/index-sections/SectionCarousel.js";
+// import SectionNucleoIcons from "views/index-sections/SectionNucleoIcons.js";
+// import SectionDark from "views/index-sections/SectionDark.js";
+// import SectionLogin from "views/index-sections/SectionLogin.js";
+// import SectionExamples from "views/index-sections/SectionExamples.js";
+// import SectionDownload from "views/index-sections/SectionDownload.js";
+
+function Index() {
+  document.documentElement.classList.remove('nav-open');
+  React.useEffect(() => {
+    document.body.classList.add('index');
+    return function cleanup() {
+      document.body.classList.remove('index');
+    };
+  });
+  return (
+    <>
+      <IndexNavbar />
+      <IndexHeader />
+      {/* <div className="main">
+        <SectionButtons />
+        <SectionNavbars />
+        <SectionNavigation />
+        <SectionProgress />
+        <SectionNotifications />
+        <SectionTypography />
+        <SectionJavaScript />
+        <SectionCarousel />
+        <SectionNucleoIcons />
+        <SectionDark />
+        <SectionLogin />
+        <SectionExamples />
+        <SectionDownload />
+        <DemoFooter />
+      </div> */}
+    </>
+  );
+}
+
+export default Index;
